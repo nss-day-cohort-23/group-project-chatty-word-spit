476ee01761085641cb6c5a0673f25b11675b5d0c
kylesaldana5 <kylesaldana5@gmail.com>
2017-12-13 12:02:34 -0600

    changed text box to range bar

--- javascripts/dominteractions.js
+++ javascripts/dominteractions.js
@@ -3,16 +3,18 @@ let printToDom = require('./printtodom');
 
 let msgInput = document.getElementById("msgInput");
 let darkCheckBox = document.getElementById("dark");
-let largeTextBox = document.getElementById("large");
+let sizeRange = document.getElementById("large");
 let clearButton = document.getElementById("clearMsgs");
 let mainWrapper = document.getElementById("mainWrapper");
 let msgOutput = document.getElementById("msgOutput");
+let textRap = document.getElementById("textSize");
 
 msgInput.addEventListener("keyup", printToDom);
 darkCheckBox.addEventListener("click", () => {
 mainWrapper.classList.toggle("darkBackground");});  
-largeTextBox.addEventListener("click", ()=>{
-mainWrapper.classList.toggle("makeItBig");});
+sizeRange.addEventListener("change", ()=>{
+textRap.setAttribute("class", `size-${sizeRange.value}`);});
+
 clearButton.addEventListener("click", () => {
     msgOutput.innerHTML = '';
     msgInput.value = ''; });
