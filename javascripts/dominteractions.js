5ade834b48d65b1ba58f638c95377a9221a5c940
kylesaldana5 <kylesaldana5@gmail.com>
2017-12-13 10:08:04 -0600

    finished DOM interactions js

--- javascripts/dominteractions.js
+++ javascripts/dominteractions.js
@@ -1,12 +1,18 @@
 "use strict";
-require('./printtodom');
+let printToDom = require('./printtodom');
 
 let msgInput = document.getElementById("msgInput");
 let darkCheckBox = document.getElementById("dark");
 let largeTextBox = document.getElementById("large");
 let clearButton = document.getElementById("clearMsgs");
+let mainWrapper = document.getElementById("mainWrapper");
+let msgOutput = document.getElementById("msgOutput");
 
-msgInput.addEventListener("keyup", arrayToMsg);
-darkCheckBox.addEventListener("check");
-largeTextBox.addEventListener("check");
-clearButton.addEventListener("click");
+msgInput.addEventListener("keyup", printToDom);
+darkCheckBox.addEventListener("click", () => {
+mainWrapper.classList.toggle("darkBackground");});  
+largeTextBox.addEventListener("click", ()=>{
+mainWrapper.classList.toggle("makeItBig");});
+clearButton.addEventListener("click", () => {
+    msgOutput.innerHTML = '';
+    msgInput.value = ''; });
