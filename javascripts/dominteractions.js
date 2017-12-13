f57fa0ff925e9439af9449b7435358acd5102709
kylesaldana5 <kylesaldana5@gmail.com>
2017-12-13 12:53:13 -0600

    made drop down themes /  styled buttons

--- javascripts/dominteractions.js
+++ javascripts/dominteractions.js
@@ -2,7 +2,7 @@
 let printToDom = require('./printtodom');
 
 let msgInput = document.getElementById("msgInput");
-let darkCheckBox = document.getElementById("dark");
+let themesBox = document.getElementById("themes");
 let sizeRange = document.getElementById("large");
 let clearButton = document.getElementById("clearMsgs");
 let mainWrapper = document.getElementById("mainWrapper");
@@ -10,11 +10,10 @@ let msgOutput = document.getElementById("msgOutput");
 let textRap = document.getElementById("textSize");
 
 msgInput.addEventListener("keyup", printToDom);
-darkCheckBox.addEventListener("click", () => {
-mainWrapper.classList.toggle("darkBackground");});  
+themesBox.addEventListener("change", () => {
+mainWrapper.setAttribute("class", `theme-${themesBox.value}`);});    
 sizeRange.addEventListener("change", ()=>{
 textRap.setAttribute("class", `size-${sizeRange.value}`);});
-
 clearButton.addEventListener("click", () => {
     msgOutput.innerHTML = '';
     msgInput.value = ''; });
