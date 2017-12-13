390805fab4244e0fb0986d1ba3b03e0f7b0951b6
kylesaldana5 <kylesaldana5@gmail.com>
2017-12-13 08:47:43 -0600

    event listners

--- javascripts/dominteractions.js
+++ javascripts/dominteractions.js
@@ -1,2 +1,12 @@
 "use strict";
 require('./printtodom');
+
+let msgInput = document.getElementById("msgInput");
+let darkCheckBox = document.getElementById("dark");
+let largeTextBox = document.getElementById("large");
+let clearButton = document.getElementById("clearMsgs");
+
+msgInput.addEventListener("keyup", arrayToMsg);
+darkCheckBox.addEventListener("check");
+largeTextBox.addEventListener("check");
+clearButton.addEventListener("click");
