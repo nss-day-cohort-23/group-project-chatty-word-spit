2ba1d2fda4cf912089fabe1111ff3b8d232478cc
Eduardo DuQuesne <eduquesne@Eduardos-MacBook-Pro.local>
2017-12-14 14:12:10 -0600

    Retrieve files from Firebase

--- javascripts/xhr.js
+++ javascripts/xhr.js
@@ -1,16 +1,19 @@
 "use strict";
+
+
 let manipulateData = require('./manipulatexhrdata');
 
 let msgRequest = new XMLHttpRequest();
 
 const parseMsgData = () => {
-    const defaultMsgArray = JSON.parse(event.target.responseText).messages;
+    console.log(event.target.responseText);
+    const defaultMsgArray = JSON.parse(event.target.responseText);
     manipulateData(defaultMsgArray);
     
 };
 
 msgRequest.addEventListener("load", parseMsgData);
-msgRequest.open("GET", "/JSON/rapbattle.json");
+msgRequest.open("GET", "https://word-spit.firebaseio.com/messages/messages.json");
 msgRequest.send();
 
 
