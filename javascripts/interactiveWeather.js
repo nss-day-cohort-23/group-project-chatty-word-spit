8c81ce5773a3c913f81558b810c4d9ef25b61cf5
Melissa Bell <melissabell456@gmail.com>
2017-12-15 06:21:54 -0600

    increased flexibility for interactive API. added wind chill and wind stats

--- javascripts/interactiveWeather.js
+++ javascripts/interactiveWeather.js
@@ -4,11 +4,15 @@ let printToDom = require("./printtodom");
 let currentTempForUser;
 let currentTimeForUser;
 let currentPrecipForUser;
+let currentWindForUser;
+let currentWindChillForUser;
 
-let weatherResponse = (weather, time, precipitation) => {
+let weatherResponse = (weather, time, precipitation, wind, windChill) => {
     currentTempForUser = weather;
     currentTimeForUser = time;
     currentPrecipForUser = precipitation;
+    currentWindForUser = wind;
+    currentWindChillForUser = windChill;
 };
 
 //adding in dummy argument//
@@ -16,7 +20,7 @@ let getUserWeather = (userEntry) => {
     let a = "API";
     let u = "user";
     console.log(userEntry);
-    if (userEntry.includes("weather")) {
+    if (userEntry.includes("weather") || userEntry.includes("temperature")) {
         printToDom.createMsgDiv(userEntry, u);
         printToDom.createMsgDiv(currentTempForUser, a);
     } 
@@ -27,6 +31,14 @@ let getUserWeather = (userEntry) => {
     else if (userEntry.includes("rain") || userEntry.includes("snow")) {
         printToDom.createMsgDiv(userEntry, u);
         printToDom.createMsgDiv(currentPrecipForUser, a);
+    }
+    else if (userEntry.includes("wind") && userEntry.includes("chill")) {
+        printToDom.createMsgDiv(userEntry, u);
+        printToDom.createMsgDiv(currentWindChillForUser, a);
+    }
+    else if (userEntry.includes("wind")) {
+        printToDom.createMsgDiv(userEntry, u);
+        printToDom.createMsgDiv(currentWindForUser, a);
     }
     else {
         printToDom.createMsgDiv(userEntry, u);
